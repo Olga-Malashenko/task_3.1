9eea6262e20f4ab5e2b30264693dfc8ebf41e0c6
Olga Malashenko <olga_birjukova@mail.ru>
2022-01-27 01:20:03 +0300

    File RadioTest updated. Complilation error

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -3,6 +3,23 @@ package ru.netology.radio;
 public class Radio {
 
     private int currentStation;
+    private int maxStationsNumber = 10;
+    private int maxStation = maxStationsNumber - 1; // не понимаю пока,
+    // куда эту строчку воткнуть, чтоб не дублировать
+
+    public Radio(int maxStationsNumber) {
+        this.maxStationsNumber = maxStationsNumber;
+    }
+    public Radio() {
+    }
+
+    public int getToMaxStation() {
+        return maxStation;
+    }
+
+    public void setToMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
 
     public int getCurrentStation() {
         return currentStation;
@@ -12,57 +29,57 @@ public class Radio {
         if (currentStation < 0) {
             return;
         }
-        if (currentStation > 9) {
+        if (currentStation > maxStation) {
             return;
         }
         this.currentStation = currentStation;
     }
 
-    public void setToNextStation() {
-        if (currentStation < 9) {
-            this.currentStation += 1;
-            return;
-        }
-        this.currentStation = 0;
-    }
-
-    public void setToPrevStation() {
-        if (currentStation > 0) {
-            this.currentStation -= 1;
-        }
-        else this.currentStation = 9;
-    }
-
-    private int currentVolume;
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) {
-            return;
-        }
-        if (currentVolume > 10) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-    // В задании нет этого метода,
-    // но я пришла к выводу, что он нобходим. Ход мыслей к комментариях к тесту.
-    // Правильные ли там рассуждения, или я до чего-то не додумалась?
-
-    public void increaseVolume() {
-        if (currentVolume == 10) {
-            return;
-        }
-        this.currentVolume += 1;
-    }
-
-    public void decreaseVolume() {
-        if (currentVolume == 0) {
-            return;
-        }
-        this.currentVolume -= 1;
-    }
+//    public void setToNextStation() {
+//        if (currentStation < 9) {
+//            this.currentStation += 1;
+//            return;
+//        }
+//        this.currentStation = 0;
+//    }
+//
+//    public void setToPrevStation() {
+//        if (currentStation > 0) {
+//            this.currentStation -= 1;
+//        }
+//        else this.currentStation = 9;
+//    }
+//
+//    private int currentVolume;
+//
+//    public int getCurrentVolume() {
+//        return currentVolume;
+//    }
+//
+//    public void setCurrentVolume(int currentVolume) {
+//        if (currentVolume < 0) {
+//            return;
+//        }
+//        if (currentVolume > 10) {
+//            return;
+//        }
+//        this.currentVolume = currentVolume;
+//    }
+//    // В задании нет этого метода,
+//    // но я пришла к выводу, что он нобходим. Ход мыслей к комментариях к тесту.
+//    // Правильные ли там рассуждения, или я до чего-то не додумалась?
+//
+//    public void increaseVolume() {
+//        if (currentVolume == 10) {
+//            return;
+//        }
+//        this.currentVolume += 1;
+//    }
+//
+//    public void decreaseVolume() {
+//        if (currentVolume == 0) {
+//            return;
+//        }
+//        this.currentVolume -= 1;
+//    }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -9,72 +9,92 @@ import static org.junit.jupiter.api.Assertions.*;
 class RadioTest {
 
     @ParameterizedTest
+    @CsvFileSource(resources = "/dataStationsNumber.csv")
+    void shouldUseConstructer(String test, int maxStationsNumber, int expected) {
+        Radio radio = new Radio(maxStationsNumber);
+        //int maxStation = maxStationsNumber - 1; // мне не нравится, что я дублирую логику
+        radio.setToMaxStation(maxStationsNumber);
+
+        int actual = radio.getToMaxStation();
+
+        assertEquals(expected, actual);
+    }
+
+
+
+    @ParameterizedTest
     @CsvFileSource(resources = "/data1.csv")
-    void shouldSetCurrentStation(String test, int currentStation, int expected) {
+    void shouldSetCurrentStationForDefolt(String test, int currentStation, int expected) {
         Radio radio = new Radio();
         radio.setCurrentStation(currentStation);
-
         int actual = radio.getCurrentStation();
-
         assertEquals(expected, actual);
     }
-
     @ParameterizedTest
-    @CsvFileSource(resources = "/data2.csv")
-    void shouldSetToNextStation(String test, int currentStation, int expected) {
-        Radio radio = new Radio();
+    @CsvFileSource(resources = "/data1-2.csv")
+    void shouldSetCurrentStationForSetting(String test, int currentStation, int expected, int maxStationsNumber) {
+        Radio radio = new Radio(maxStationsNumber);
         radio.setCurrentStation(currentStation);
-        radio.setToNextStation();
-
         int actual = radio.getCurrentStation();
-
         assertEquals(expected, actual);
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data3.csv")
-    void setToPrevStation(String test, int currentStation, int expected) {
-        Radio radio = new Radio();
-        radio.setCurrentStation(currentStation);
-        radio.setToPrevStation();
-
-        int actual = radio.getCurrentStation();
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data4.csv")
-    void shouldSetCurrentVolume(String test, int currentVolume, int expected) {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(currentVolume);
-
-        int actual = radio.getCurrentVolume();
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data5.csv")
-    void shouldIncreaseVolume(String test, int currentVolume, int expected) {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(currentVolume);
-        radio.increaseVolume();
-
-        int actual = radio.getCurrentVolume();
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/data6.csv")
-    void shouldDecreaseVolume(String test, int currentVolume, int expected) {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(currentVolume);
-        radio.decreaseVolume();
-
-        int actual = radio.getCurrentVolume();
-
-        assertEquals(expected, actual);
-    }
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data2.csv")
+//    void shouldSetToNextStation(String test, int currentStation, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentStation(currentStation);
+//        radio.setToNextStation();
+//
+//        int actual = radio.getCurrentStation();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data3.csv")
+//    void setToPrevStation(String test, int currentStation, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentStation(currentStation);
+//        radio.setToPrevStation();
+//
+//        int actual = radio.getCurrentStation();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data4.csv")
+//    void shouldSetCurrentVolume(String test, int currentVolume, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentVolume(currentVolume);
+//
+//        int actual = radio.getCurrentVolume();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data5.csv")
+//    void shouldIncreaseVolume(String test, int currentVolume, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentVolume(currentVolume);
+//        radio.increaseVolume();
+//
+//        int actual = radio.getCurrentVolume();
+//
+//        assertEquals(expected, actual);
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/data6.csv")
+//    void shouldDecreaseVolume(String test, int currentVolume, int expected) {
+//        Radio radio = new Radio();
+//        radio.setCurrentVolume(currentVolume);
+//        radio.decreaseVolume();
+//
+//        int actual = radio.getCurrentVolume();
+//
+//        assertEquals(expected, actual);
+//    }
 }
